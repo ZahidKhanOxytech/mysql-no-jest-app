23ad36ecbd804b271f852ae5565135e17c107689
Zahid Khan <159260833+ZahidKhanOxytech@users.noreply.github.com>
2024-10-16 14:01:28 +0530

    Auto-commit via Make.com

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -102,7 +102,7 @@ exports.removeItem = async (req, res) => {
     await client.del(`items`);
 
     res.status(200);
-    res.json({ message: "Item removed successfully21" });
+    res.json({ message: "Item removed successfully22" });
   } catch (err) {
     res.status(500);
     res.json({ message: err.message });
